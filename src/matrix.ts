09d00b40ac608177b246b35bca83e21780a3d13d
tianxiaoyang <tianxiaoyang@cxxmobi.com>
2020-09-24 17:52:53 +0800

    添加矩阵加法，矩阵乘法与哈达玛积

--- src/matrix.ts
+++ src/matrix.ts
@@ -7,7 +7,6 @@ export default class Matrix<R extends number = 1, C extends number = 1> {
         r = r && r > 0 ? r : 1 as R;
         c = c && c > 0 ? c : 1 as C;
         this.#data = data ? data.concat() : [];
-        this.#data.length = r * c;
         this.#r = r;
         this.#c = c;
     }
@@ -47,12 +46,27 @@ export default class Matrix<R extends number = 1, C extends number = 1> {
         return lines.join('\n');
     }
 
-    reshape<NewR extends number, NewC extends number>(r: NewR, c: NewC) {
-        if (this.#r * this.#c % c !== 0) {
-            throw RangeError(`Can't reshape to ${c} columms`);
+    getRow(r: number) {
+        if (r < 0 || r >= this.#r) throw RangeError("wrong row index");
+        let res = new Matrix(1, this.#c);
+        for (let i = 0; i < this.#c; ++i) {
+            res.#data[i] = this.val(i, r);
         }
+        return res;
+    }
+
+    getColumn(c: number) {
+        if (c < 0 || c >= this.#c) throw RangeError("wrong column index");
+        let res = new Matrix(this.#r, 1);
+        for (let i = 0; i < this.#r; ++i) {
+            res.#data[i] = this.val(c, i);
+        }
+        return res;
+    }
+
+    reshape<NewR extends number, NewC extends number>(r: NewR, c: NewC) {
         let mat: Matrix<NewR, NewC>;
-        if (r === this.#r * this.#c / c) {
+        if (r * c === this.#r * this.#c) {
             mat = new Matrix(r, c);
             mat.#data = this.#data;
         }
@@ -60,5 +74,39 @@ export default class Matrix<R extends number = 1, C extends number = 1> {
             mat = new Matrix(r, c, this.#data);
         }
         return mat;
+    }
+
+    add(mat: Matrix<R, C>) {
+        let res = new Matrix(this.#r, this.#c);
+        const dlen = this.#r * this.#c;
+        for (let i = 0; i < dlen; ++i) {
+            res.#data[i] = this.#data[i] + mat.#data[i];
+        }
+        return res;
+    }
+
+    multiply<NewC extends number>(mat: Matrix<C, NewC>) {
+        let res = new Matrix(this.#r, mat.#c);
+        for (let r = 0; r < this.#r; ++r) {
+            for (let c = 0; c < mat.#c; ++c) {
+                let left = this.getRow(r);
+                let right = mat.getColumn(c);
+                let product = 0;
+                for (let i = 0; i < this.#c; ++i) {
+                    product += left.val(i, 0) * right.val(0, i);
+                }
+                res.#data[r * mat.#c + c] = product;
+            }
+        }
+        return res;
+    }
+
+    hadamard(mat: Matrix<R, C>): Matrix<R, C> {
+        let res = new Matrix(this.#r, this.#c);
+        const dlen = this.#r * this.#c;
+        for (let i = 0; i < dlen; ++i) {
+            res.#data[i] = this.#data[i] * mat.#data[i];
+        }
+        return res;
     }
 }
--- test/matrix.test.ts
+++ test/matrix.test.ts
@@ -17,12 +17,58 @@ describe("Matrix testing", function () {
         let { rows, columns } = mat;
         assert.deepEqual([rows, columns], [3, 2]);
     });
+
+    let shapeMat = new Matrix(3, 2, [1, 2, 3, 4, 5, 6]);
     it('reshape matrix', function () {
-        let mat = new Matrix(3, 2, [1, 2, 3, 4, 5, 6]);
-        assert.equal(String(mat.reshape(2, 3)), '|1,2,3|\n|4,5,6|');
+        assert.equal(String(shapeMat.reshape(2, 3)), '|1,2,3|\n|4,5,6|');
     });
     it('resize matrix', function () {
-        let mat = new Matrix(3, 2, [1, 2, 3, 4, 5, 6]);
-        assert.equal(String(mat.reshape(3, 3)), '|1,2,3|\n|4,5,6|\n|0,0,0|');
+        assert.equal(String(shapeMat.reshape(2, 5)), '|1,2,3,4,5|\n|6,0,0,0,0|');
+    });
+
+    let dataMat = new Matrix(3, 5, [
+        4, 7, 8, 1, 6,
+        9, 4, 2, 3, 7,
+        5, 0, 3, 2, 4,
+    ]);
+    it('row vector', function () {
+        assert.equal(String(dataMat.getRow(1)), '|9,4,2,3,7|');
+    });
+    it('get wrong row', function () {
+        assert.throw(() => dataMat.getRow(3), 'wrong row index');
+    });
+    it('column vector', function () {
+        assert.equal(String(dataMat.getColumn(3)), '|1|\n|3|\n|2|');
+    });
+    it('column single vector', function () {
+        let vec = new Matrix(3, 1, [1, 2, 3])
+        assert.equal(String(vec.getColumn(0)), '|1|\n|2|\n|3|');
+    });
+    it('get wrong column', function () {
+        assert.throw(() => dataMat.getColumn(-1), 'wrong column index');
+    });
+});
+
+describe("Matrix caculate", function () {
+    it('matrix addition', function () {
+        let left = new Matrix(2, 2, [1, 2, 3, 4]);
+        let right = new Matrix(2, 2, [3, 7, 5, 4]);
+        assert.equal(String(left.add(right)), '|4,9|\n|8,8|');
+    });
+    it('vector multiply', function () {
+        let left = new Matrix(1, 3, [1, 2, 3]);
+        let right = new Matrix(3, 1, [1, 2, 3]);
+        assert.equal(Number(left.multiply(right)), 14);
+    });
+    it('matrix multiply', function () {
+        let left = new Matrix(2, 3, [1, 2, 3, 4, 5, 6]);
+        let right = new Matrix(3, 2, [1, 4, 2, 5, 3, 6]);
+        assert.equal(String(left.multiply(right)), '|14,32|\n|32,77|');
+    });
+
+    it('hadamard product', function () {
+        let left = new Matrix(2, 2, [1, 2, 4, 5]);
+        let right = new Matrix(2, 2, [1, 4, 2, 5]);
+        assert.equal(String(left.hadamard(right)), '|1,8|\n|8,25|');
     })
 });
